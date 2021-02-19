f4f23517f82930b247796f6242553f3e9a77883f
Andrew Dyer <andrewdyer@outlook.com>
2021-02-19 00:57:22 +0000

    :white_check_mark: Test can send message

--- tests/MailerTest.php
+++ tests/MailerTest.php
@@ -3,6 +3,7 @@
 namespace Anddye\Mailer\Tests;
 
 use Anddye\Mailer\Mailer;
+use Anddye\Mailer\MessageBuilder;
 use PHPUnit\Framework\TestCase;
 use Slim\Views\Twig;
 
@@ -25,5 +26,27 @@ final class MailerTest extends TestCase
         $this->assertEquals($protocol, $transport->getEncryption());
         $this->assertEquals($username, $transport->getUsername());
         $this->assertEquals($password, $transport->getPassword());
+    }
+
+    public function testCanSendMessage(): void
+    {
+        $address = getenv('MAILER_DEFAULT_FROM_ADDRESS');
+        $name = getenv('MAILER_DEFAULT_FROM_NAME');
+
+        $mailer = new Mailer(new Twig(__DIR__), [
+            'host' => getenv('MAILER_HOST'),
+            'port' => getenv('MAILER_PORT'),
+            'username' => getenv('MAILER_USERNAME'),
+            'password' => getenv('MAILER_PASSWORD'),
+        ]);
+
+        $mailer->setDefaultFrom($address, $name);
+
+        $sent = $mailer->sendMessage('views/mailer.twig', [], function (MessageBuilder $messageBuilder) use ($address, $name) {
+            $messageBuilder->setTo($address, $name);
+            $messageBuilder->setSubject('Welcome to the Team!');
+        });
+
+        $this->assertEquals(1, $sent);
     }
 }
